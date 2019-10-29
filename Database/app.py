3769515b7686b71abf686618924e6fb42e4713ac
Stavros Burke <Stavrosburke@live.com>
2019-10-29 15:55:58 -0500

    Add changed to databse
    Database is able to accept data as an argument, still need to tweek pandas dataframe

--- Database/app.py
+++ Database/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+import pandas as pd
+import MySQLdb
+import pandas.io.sql as psql
 import os
 
 
@@ -11,6 +14,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
 
+#check if database exists
+#generate databaseq
 class MLData(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	user = db.Column(db.String(20))
@@ -21,3 +26,34 @@ class MLData(db.Model):
 	day = db.Column(db.String(40))
 	holiday=db.Column(db.String(40))
 
+
+
+def addData(user,address,action,timestamp,date,day,holiday):
+	entry = MLData(user=user,address=address,action=action,timestamp=timestamp,date=date,day=day,holiday=holiday)
+	db.session.add(entry)
+	db.session.commit()	
+
+addData("Setev","1011 home","turn on light","10:34:35","10-24-30","Monday","0")
+
+
+
+
+
+# setup the database connection.  There's no need to setup cursors with pandas psql.
+datab=MySQLdb.connect(host=HOST, user=USER, passwd=PW, datab=db)
+
+# create the query
+query = "select * from TABLENAME"
+
+# execute the query and assign it to a pandas dataframe
+df = psql.read_sql(query, con=datab)
+# close the database connection
+datab.close()
+
+
+
+#make function to accept data from ML code and add to DB
+
+#take all the data from DB and turn it into panda shit.
+
+#get in contact with Derrick about scripts.
