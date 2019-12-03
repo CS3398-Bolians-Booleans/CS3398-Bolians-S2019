105204e27b7282c6abd09699049b4c35eca521b4
1SHERWIN <31481757+1SHERWIN@users.noreply.github.com>
2019-12-03 10:53:01 -0600

    Traffic API needed import requests

--- custom_components/box1/Traffic.py
+++ custom_components/box1/Traffic.py
@@ -1,42 +1,38 @@
 # 2019 Nov
-# Author Sherwin Massoudian
+# Author: Sherwin Massoudian
 
-# Usage:
-# Traffic.getTraffic()
+# Usage: Traffic.getTraffic()
 
-# Gets current travel time - average
-# example: 
+# Output is seconds in traffic betweem 2 locations
 # 410
 
-import random
+
+import requests, json
 
 class Traffic: 
-    response = { "destination_addresses" : [ "San Marcos, TX, USA" ], "origin_addresses" : [ "Austin, TX, USA" ], "rows" : [ { "elements" : [ { "distance" : { "text" : "40 mi", "value" : 64373.8 }, "duration" : { "text" : "0 hours 45 mins", "value" : 2700 }, "status" : "OK" } ] } ], "status" : "OK" }
-	
-	
+    backupResponse = { "destination_addresses" : [ "San Marcos, TX, USA" ], "origin_addresses" : [ "Austin, TX, USA" ], "rows" : [ { "elements" : [ { "distance" : { "text" : "40 mi", "value" : 64373.8 }, "duration" : { "text" : "0 hours 45 mins", "value" : 2700 }, "status" : "OK" } ] } ], "status" : "OK" }
     # This method returns traffic time in seconds
-    def getTraffic(self, homeAddress, SchoolAddress): 
-        
-		# Get travel time from payload
-		rowArr = self.response['rows']
+    def getTraffic(self, source, destination):
+        api_key = 'AIzaSyAyDDKXONfTxf1Cj4I-oB1b3bE4ZIictgE'
+        url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+        # Get travel time from payload
+        payload = requests.get(url + 'origins = ' + source + '&destinations = ' + destination + '&key = ' + api_key)	
+        response = payload.json()
+        response = self.backupResponse
+        rowArr = response['rows']
         rows = rowArr[0]
         elementArr = rows['elements']
         elements = elementArr[0]
         duration = elements['duration']
-        currentTravelTime = duration['value']
-		
-		# Backup
-		travelTime = random.randrange(2500, 3100)
-        averageTime = self.getAvg();
-        
-        return travelTime - averageTime
+        currentTravelTime = duration['value'] 
+        return currentTravelTime - self.getAvg()
     
 	# This method returns the average traffic time
     def getAvg(self): 
         SanMarcos2Austin = 2420
         return SanMarcos2Austin
 	
-	# Method to print the traffic time to file
+	# Prints the traffic time to file
 	# def printTime(self):
         # Output to a text file
         # file = open("trafficTime.txt","w")
@@ -45,4 +41,4 @@ class Traffic:
 
 # Sample usage
 Austin = Traffic()
-print ("Expected time in traffic:", Austin.getTraffic(),"seconds")
+print ("Expected time in traffic:", Austin.getTraffic('Austin', 'Texas State University'),"seconds")
